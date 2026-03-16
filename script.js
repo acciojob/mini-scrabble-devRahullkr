6fcd06170dc4eaebfa9096070456319c9657ae58
Rahul Kr <rahulkumar8604745@gmail.com>
2026-03-16 10:21:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const letter = document.querySelector("#evaluatedText")
+const showLetter = document.querySelector("#letterCount")
+
+letter.addEventListener("input",function(){
+	const length = this.value.length;
+	showLetter.textContent = length;
+})
